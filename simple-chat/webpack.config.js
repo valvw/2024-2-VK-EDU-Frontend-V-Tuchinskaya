b7e02cc9cb246c83509cb94f226e07d699c7d398
Val <33302737+valvw@users.noreply.github.com>
2024-10-07 17:00:30 +0300

    update webpack.config.js

--- simple-chat/webpack.config.js
+++ simple-chat/webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "build"),
-    publicPath: "/",
+    publicPath: "./",
     filename: "bundle.js",
   },
 
